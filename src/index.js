50cf3c8ba4a7a9d54262bfa89e9fd5841edad16a
Chris Ng <chris.ng@scmp.com>
2018-11-13 11:27:05 +0800

    #6 add onload option for idlequeue call

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 import idleQueueHelper from './helpers/idleQueue'
 import onLoadIdleQueue from './components/onLoadIdleQueue.js'
 import idleQueue from './components/idleQueue.js'
+import get from 'lodash/get'
+import defaults from 'lodash/defaults'
 
 export default {
   install (Vue, options = {}) {
-    Vue.component('onload-idle-queue', onLoadIdleQueue(options))
+    const defaultOptions = {
+      onload: false,
+      onLoadTimeout: 0,
+    }
+
+    Vue.component('onload-idle-queue', onLoadIdleQueue(defaults(defaultOptions, options)))
     Vue.component('idle-queue', idleQueue)
 
-    Vue.prototype.$idleQueue = function (func) {
-      idleQueueHelper(func)
+    Vue.prototype.$idleQueue = function (func, options = {}) {
+      options = defaults(defaultOptions, options)
+      if (get(options, 'onload') && typeof window !== 'undefined' && typeof document !== 'undefined') {
+        if (get(document, 'readyState') === 'complete') {
+          idleQueueHelper(func)
+        } else {
+          let timeoutID = null
+          const setOnLoad = () => {
+            if (timeoutID) {
+              clearTimeout(timeoutID)
+              timeoutID = null
+            }
+            idleQueueHelper(func)
+            window.removeEventListener('load', setOnLoad)
+          }
+          window.addEventListener('load', setOnLoad)
+          if (get(options, 'onLoadTimeout') > 0) {
+            timeoutID = setTimeout(setOnLoad, get(options, 'onLoadTimeout'))
+          }
+        }
+      } else {
+        idleQueueHelper(func)
+      }
     }
   }
 }
