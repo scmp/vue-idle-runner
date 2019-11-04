c3ae7b9bb31e461f44d57df4bc549181b1a667c7
Jeff Leung <jeff.leung@scmp.com>
2019-11-04 15:58:59 +0800

    1.2.1

--- dist/components/idleQueue.js
+++ dist/components/idleQueue.js
@@ -13,8 +13,14 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 exports.default = {
   name: 'idle-queue',
   mixins: [_idle2.default],
+  props: {
+    forceRender: {
+      type: Boolean,
+      default: false
+    }
+  },
   render: function render(h) {
-    if (this.isIdle) {
+    if (this.forceRender || this.isIdle) {
       return h('div', null, this.$slots.default);
     }
     return h;
