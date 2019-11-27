85ae3fab5648cb04da177ed1b5ea8df56484bbf2
Quintus Leung <quintus.leung@scmp.com>
2019-11-27 17:10:07 +0800

    Update type declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,8 +4,13 @@ declare module 'vue-idle-runner' {
   export const install: PluginFunction<never>
 }
 
+declare module 'vue-idle-runner/mixins' {
+  export const idle: Vue;
+  export const onLoadIdle: Vue;
+}
+
 declare module "vue/types/vue" {
   interface Vue {
-    $idleQueue: (() => boolean)
+    $idleQueue: (cb: () => void) => boolean;
   }
 }
